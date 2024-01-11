6bd19475e7688a53067eb67a48c1094e82bfe326
WooBinJang <jwb449730@naver.com>
2024-01-11 17:28:02 +0900

    next file upload & Cloudinary use

---
+++ app/upload/page.tsx
@@ -0,0 +1,38 @@
+"use client";
+import { CldImage, CldUploadWidget } from "next-cloudinary";
+import React, { useState } from "react";
+
+interface CloudinaryResult {
+  public_id: string;
+}
+const UpdatePage = () => {
+  const [publicId, setPublicId] = useState("");
+  return (
+    <>
+      {publicId && (
+        <CldImage
+          src={publicId}
+          width={270}
+          height={180}
+          alt="upload image"
+        ></CldImage>
+      )}
+      <CldUploadWidget
+        uploadPreset="lxgegls1"
+        onUpload={(result, widget) => {
+          if (result.event !== "success") return;
+          const info = result.info as CloudinaryResult;
+          setPublicId(info.public_id);
+        }}
+      >
+        {({ open }) => (
+          <button className="btn btn-primary" onClick={() => open()}>
+            Upload
+          </button>
+        )}
+      </CldUploadWidget>
+    </>
+  );
+};
+
+export default UpdatePage;
